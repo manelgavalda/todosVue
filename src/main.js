96e9e1c02490bf4fe7e68143d2d31b970ca08c55
manel <manelgavalda@iesebre.com>
2017-01-13 17:04:53 +0100

    Compiling

--- src/main.js
+++ src/main.js
@@ -6,19 +6,19 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import VueRouter from 'vue-router'
 import Tokens from 'components/Tokens'
-import Todos from 'components/Tokens'
+import Todos from 'components/Todos'
 
 const routes = [
     { path: '/foo', component: Todos },
     { path: '/bar', component: Tokens }
 ]
 
-const routes = new VueRouter({
-  routes
-})
+const router = new VueRouter(
+  routes // short for routes: routes
+)
 
 Vue.use(VueMaterial)
-Vue.use(VueRouter)
+Vue.use(router)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
