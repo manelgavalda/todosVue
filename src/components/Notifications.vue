55ca7b53fff303799ec1e9c7d32523edf389eff3
manelgavalda <manelgavalda@iesebre.com>
2017-04-24 14:43:47 +0200

    CAnnot get messages, lazy loadgin not working

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -12,25 +12,59 @@
 <style>
 </style>
 <script>
-//  import todosVue from '../todosVue'
+  import todosVue from '../todosVue'
   import notifications from '../services/notifications'
   export default{
+    data () {
+      return {
+        notifications: [],
+        connecting: false,
+        config: {
+          errorLabel: 'Error',
+          startLabel: 'Swipe',
+          readyLabel: 'Release',
+          loadingLabel: 'Refreshing...'
+        }
+      }
+    },
     created () {
+      var that = this
+      setTimeout(function () {
+        that.fetchPage()
+        that.connecting = false
+      }, 500)
       document.addEventListener('deviceready', this.onDeviceReady, false)
-      notifications.enable()
+      if (window.cordova && window.device.platform !== 'browser') {
+        notifications.enable()
+      }
     },
     methods: {
       onDeviceReady  () {
         console.log('Working on platform' + window.device.platform)
         console.log('Data')
-      }
-    },
-    props: {
-      notifications: {
-        type: Array,
-        default: function () {
-          return []
-        }
+      },
+      fetchPage: function () {
+        this.$http.get(todosVue.GET_MESSAGES_URL).then((response) => {
+          console.log(response)
+          this.notifications = response.data
+        }, (err) => {
+          console.log(err)
+        })
+      },
+      showConnectionError () {
+        this.$refs.connectionError.open()
+      },
+      onRefresh: function () {
+        this.connecting = true
+        return new Promise(function (resolve, reject) {
+          setTimeout(function () {
+            resolve()
+          }, 1000)
+        }).then(() => {
+          this.fetchData()
+        }).catch(() => {
+          this.showConnectionError()
+        })
       }
     }
   }
